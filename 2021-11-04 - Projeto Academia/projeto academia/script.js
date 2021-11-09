aa43ce34d7c4f7ae78c055240e52defcf6878eee
ppedraum <pedrovirtuozo@gmail.com>
2021-11-09 17:21:30 -0300

    perdi a vontade de fazer isso
    perdi a vontade de fazer tudo

--- 2021-11-04 - Projeto Academia/projeto academia/script.js
+++ 2021-11-04 - Projeto Academia/projeto academia/script.js
@@ -22,3 +22,37 @@ startList = function()
   }
 }
 window.onload=startList;
+
+var slides=new Array("equipe/Cleo.JFIF","equipe/Dennis.JFIF","equipe/Mark.JFIF");
+var tam=slides.length;
+var satual=1;
+var tmpSlider;
+
+function trocaAutomatica(){
+	satual++;
+	if(satual >= tam){
+		satual=0;
+	}
+  document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+}
+
+function iniciaSlider(){
+	document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+	tmpSlider=setInterval(trocaAutomatica,3000);
+}
+
+function parar(){
+	clearInterval(tmpSlider);
+}
+
+function troca(dir){
+	satual+=dir;
+	if(satual < 0){
+		satual=2;
+	}else if(satual >= tam){
+		satual=0;
+	}
+	document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+	clearInterval(tmpSlider);
+	tmpSlider=setInterval(trocaAutomatica,3000);
+}
